12aae1b1498bf11bd30409a2c78f209a6374454d
kimtaesu <kimtaesoo188@gmail.com>
2019-11-06 09:55:28 +0900

    First

---
+++ BlastingKakao/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BlastingKakao
+//
+//  Created by tskim on 2019/11/06.
+//  Copyright © 2019 assin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
