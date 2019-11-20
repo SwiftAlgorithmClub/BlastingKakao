2046ba46ea79a06f67b310a974c0bfcfcf4aaf48
koo <rnwgkur4604@interpark.com>
2019-11-20 11:24:00 +0900

    캐시 문제 푸는 중

---
+++ girin.playground/Pages/[2018][kakao]캐시.xcplaygroundpage/Contents.swift
@@ -0,0 +1,48 @@
+
+import Foundation
+
+func solution(_ cacheSize:Int, _ cities:[String]) -> Int {
+    
+    // 0 <= cacheSize <= 30
+    // cities.count <= 100000
+    // 오직 영문
+    
+    var time = 0
+    
+    var cache: [String] = []
+    
+    for city in cities {
+    
+        if cache.contains(city.lowercased()) {
+        
+            time = time + 1
+                        
+        }
+        else {
+        
+            time = time + 5
+            
+        }
+        
+        cache.append(city.lowercased())
+        
+        if cache.count > cacheSize {
+            
+            cache.remove(at: 0)
+            
+        }
+        
+//        print(cache)
+        
+        
+    }
+
+//    print(time)
+    
+    return time
+    
+}
+
+//solution(3, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"])
+
+solution(2, ["Jeju", "Pangyo", "NewYork", "newyork"])
