d84f2f5c6355238b19c7f891859f5268eddd3a84
koo <rnwgkur4604@interpark.com>
2019-11-27 17:36:57 +0900

    후보키 저장

--- girin.playground/Pages/[2019][kakao]후보키.xcplaygroundpage/Contents.swift
+++ girin.playground/Pages/[2019][kakao]후보키.xcplaygroundpage/Contents.swift
@@ -13,5 +13,28 @@ func solution(_ relation:[[String]]) -> Int {
     
 }
 
+func isEqualToArray(_ a:[String], _ b:[String]) -> Bool {
+
+    if a.count == b.count {
+    
+        for i in 0..<a.count {
+            
+            if a[i] != b[i] {
+                
+                return false
+            }
+            
+        }
+        
+        return true
+        
+    }
+    
+    return false
+    
+}
+
 
 solution([["100", "ryan", "music", "2"], ["200", "apeach", "math", "2"], ["300", "tube", "computer", "3"], ["400", "con", "computer", "4"], ["500", "muzi", "music", "3"], ["600", "apeach", "music", "2"]])
+
+
